6cf8eacbedb4143ac84dfb0e4d6195e01d26fbe5
ObukhovVladislav <vobukhov10@gmail.com>
2021-06-06 16:45:41 +0500

    game-store

--- game_store/mainapp/urls.py
+++ game_store/mainapp/urls.py
@@ -6,6 +6,8 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.index, name='index'),
     path('genre/', mainapp.genre, name='genre'),
+    path('genre/category/<int:category_pk>/', mainapp.genre_section, name='genre_section'),
+    path('genre/game/<int:game_pk>/', mainapp.game_page, name='game_page'),
 
     path('genre/create/', mainapp.add_genre, name='add_genre'),
     path('genre/edit/<int:pk>/', mainapp.edit_genre, name='edit_genre'),
@@ -13,7 +15,5 @@ urlpatterns = [
 
     path('genre/game/add/', mainapp.add_game, name='add_game'),
     path('genre/edit/game/<int:pk>/', mainapp.edit_game, name='edit_game'),
-
-    path('genre/category/<int:category_pk>/', mainapp.genre_section, name='genre_section'),
-    path('genre/game/<int:game_pk>/', mainapp.game_page, name='game_page'),
+    path('genre/remove/game/<int:pk>/', mainapp.delete_game, name='delete_game'),
 ]
